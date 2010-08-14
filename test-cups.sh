f3c09fd7ef64c13af90056e03ae868f1e106ff14
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-08-14 16:39:58 +0200

    run evolis-printer.pl over generated dumps

--- test-cups.sh
+++ test-cups.sh
@@ -1,3 +1,6 @@
+#!/bin/sh -x
+
 ls test/*.pdf | xargs -i sh -c "./gs-cups-raster.sh < {} > {}.cups"
 ls test/*.cups | xargs -i sh -c "./cups-rastertoevolis.sh {} > {}.evolis"
+ls test/*.evolis | xargs -i ./evolis-printer.pl {}
 ls -al test/
