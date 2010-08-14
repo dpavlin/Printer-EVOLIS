9acacee0673b1e7e02421d7adf038c62cc3b2272
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-08-14 14:42:44 +0200

    generate only single svg file
    unfortunatly, png export from inkscape always dumps all visible layers,
    so front and back renderings are not correct

--- inkscape-render.pl
+++ inkscape-render.pl
@@ -6,6 +6,8 @@ use autodie;
 
 my $card_svg = 'card/ffzg-2010.svg';
 
+warn "# card template: $card_svg\n";
+
 my ($nr,$ime,$prezime) = ( qw/
 200900000042
 Dobrica
@@ -30,44 +32,39 @@ foreach my $existing ( glob $out . '*' ) {
 	unlink $existing;
 }
 
-open(my $in,     '<', $card_svg);
-open(my $print,  '>', "$out.print.svg");
-open(my $screen, '>', "$out.screen.svg");
+open(my $svg_template, '<', $card_svg);
+open(my $svg,          '>', "$out.svg");
 
-while(<$in>) {
+while(<$svg_template>) {
 
 	if ( m{($re)} ) {
 		warn "mapping $1\n";
 		s{($1)}{mapping($1)}e;
 	}
 
-	if ( s{##print##}{none} ) {
-		warn "print layer: $_\n";
-		print $print $_;
-		s{none}{inline};
-		print $screen $_;
-
-	} else {
-		print $print  $_;
-		print $screen $_;
-	}
+	print $svg $_;
 
 }
 
-close($in);
-close($print);
-close($screen);
+close($svg_template);
+close($svg);
 
 open(my $inkscape, '|-', 'inkscape --shell --without-gui');
 
 sub inkscape_export {
 	my $part = shift;
-	print $inkscape "$out.print.svg --export-area-page --export-pdf $out.$part.pdf --export-id $part\n";
-	print $inkscape "$out.screen.svg --export-png $out.png --export-dpi 300";
+
+	my $shell = "$out.svg --export-area-page --export-id $part";
+
+	print $inkscape "$shell --export-pdf $out.$part.pdf\n";
+	print $inkscape "$shell --export-png $out.$part.png --export-dpi 150\n";
 }
 
 inkscape_export 'print-front';
 inkscape_export 'print-back';
+
+# export visible
+print $inkscape "$out.svg --export-png $out.png --export-dpi 300\n";
 
 close($inkscape);
 
