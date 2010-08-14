c3d89d0417f5eb66db327425da95f07e7877b194
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-08-14 16:52:15 +0200

    document better protocol

--- evolis-driver.pl
+++ evolis-driver.pl
@@ -21,7 +21,7 @@ print "\x1BPdt;DU\r";
 print "\x1BMr;s\r";
 print "\x1BPpws;1281732635\r";
 
-# SEQUENCE RECTO
+# FIXME load card into printer
 print "\x1BSs\r";
 print "\x1BSr\r";
 
@@ -30,10 +30,13 @@ my $command_size = 11682
 print "\x1BDbc;k;2;11682;"; # bitmap data
 print "\r";
 
+# even page on two side-printing
 print "\x1BSv\r";
+
 print "\x1BPc;k;=;10\r";
 
 print "\x1BDbc;k;2;31744;"; # bitmap data
 print "\r";
 
 print "\x1BSe\r";
+print "\x00" x 64; # FIXME some padding?
