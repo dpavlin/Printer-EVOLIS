ceb4ba13f19afbf632b3e2d0cf5f39359386656d
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-08-15 18:42:40 +0200

    reopen parallel port for each command

--- scripts/evolis-command.pl
+++ scripts/evolis-command.pl
@@ -9,25 +9,32 @@ use Data::Dump qw(dump);
 my $dev = '/dev/usb/lp0';
 my $debug = 0;
 
-sysopen(my $parallel, $dev, O_RDWR | O_EXCL) || die "$dev: $!";
+my $parallel;
 
 $|=1;
 print "command> ";
 while(<STDIN>) {
 	chomp;
-	
+
 	my $send = "\e$_\r";
+
+	# XXX we need to reopen parallel port for each command
+	sysopen( my $parallel, $dev, O_RDWR | O_EXCL) || die "$dev: $!";
+
 	foreach my $byte ( split(//,$send) ) {
 		warn "#>> ",dump($byte),$/ if $debug;
 		syswrite $parallel, $byte, 1;
 	}
+
 	my $response;
 	my $byte;
 	while( sysread $parallel, $byte, 1 ) {
 		$response .= $byte;
 		warn "#<< ",dump($byte),$/ if $debug;
 	}
+	close($parallel);
+
 	print "<answer ",dump($response),"\ncommand> ";
+
 }
 
-close($parallel);
