7c5c92862a42eff5d09c0b125c08fc0b20e6e6a4
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-08-15 16:42:03 +0200

    improve documentation

--- lib/Printer/EVOLIS.pm
+++ lib/Printer/EVOLIS.pm
@@ -25,21 +25,33 @@ front and back pages separatly.
 
 =head1 SCRIPTS
 
-Current toolset sonsists of following scripts:
+Current toolset consists of following scripts:
 
 =head2 scripts/inkscape-render.pl card/template.svg 201008159999 Name Surname
 
-generate pbm files from Inkscape SVG template in C<card/>
+Generate pdf files from Inkscape SVG template in C<card/> using
+C<print-front> and C<print-back> object IDs. Layers doesn't work
+since we can't toggle visilbity easily. To print more than one
+object gruop them and change ID of group.
+
+After pdf files are created, GhostScript is used to rasterize them
+into pbm (monochrome) bitmaps.
 
 =head2 scripts/evolis-driver.pl front.pbm back.pbm > evolis.commands
 
-provides driver which generates printer commands
+Provides driver which generates printer command stream to print
+two-sided card from pbm files.
 
 =head2 scripts/evolis-simulator.pl evolis
 
-provides simulator for EVOLIS printer which is useful for development
+Simulator for EVOLIS printer commands which is useful for development.
+It creates one pbm file per page printed.
 
 =head1 EXAMPLE
+
+Following is simple walk-through from svg image in Inkscape to
+evolis command stream which can be executed in top-level directory
+of this distribution:
 
   ./scripts/inkscape-render.pl card/ffzg-2010.svg 201008159999 Ime Prezime
   ./scripts/evolis-driver.pl out/201008159999.front.pbm out/201008159999.back.pbm > evolis
