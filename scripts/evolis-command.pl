d2f0277b3e28ac5dfe3150e73ff45c4874b2407f
Dobrica Pavlinusic <dpavlin@rot13.org>
2010-08-16 16:42:53 +0200

    and use it to send commands to printer

--- scripts/evolis-command.pl
+++ scripts/evolis-command.pl
@@ -3,11 +3,12 @@
 use warnings;
 use strict;
 
-use POSIX;
 use Data::Dump qw(dump);
 use Time::HiRes;
 use Getopt::Long;
 use Term::ReadLine;
+use lib 'lib';
+use Printer::EVOLIS::Parallel;
 
 my $port = '/dev/usb/lp0';
 my $debug = 0;
@@ -19,38 +20,18 @@ GetOptions(
 
 warn "# port $port debug $debug\n";
 
-my $parallel;
-
-$|=1;
+my $parallel = Printer::EVOLIS::Parallel->new( $port );
 
 my $term = Term::ReadLine->new('EVOLIS');
 my $OUT = $term->OUT || \*STDOUT;
+
+select($OUT); $|=1;
 
 while ( defined ( $_ = $term->readline('command> ')) ) {
 	chomp;
 	my $send = "\e$_\r";
 
-	# XXX we need to reopen parallel port for each command
-	sysopen( $parallel, $port, O_RDWR | O_EXCL) || die "$port: $!";
-
-	foreach my $byte ( split(//,$send) ) {
-		warn "#>> ",dump($byte),$/ if $debug;
-		syswrite $parallel, $byte, 1;
-	}
-
-	close($parallel);
-	# XXX and between send and receive
-	sysopen( $parallel, $port, O_RDWR | O_EXCL) || die "$port: $!";
-
-	my $response;
-	while ( ! sysread $parallel, $response, 1 ) { sleep 0.1 }; # XXX wait for 1st char
-	my $byte;
-	while( sysread $parallel, $byte, 1 ) {
-		warn "#<< ",dump($byte),$/ if $debug;
-		last if $byte eq "\x00";
-		$response .= $byte;
-	}
-	close($parallel);
+	my $response = $parallel->command( $send );
 
 	$term->addhistory($_) if $response;
 
